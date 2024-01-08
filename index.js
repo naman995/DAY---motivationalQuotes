c0081fb53de99ddc7ad861a6f689f2f8aafd22dc
naman995 <rajputnaman823@gmail.com>
2024-01-09 02:19:33 +0530

    DAY1 : Motivational Quotes

---
+++ index.js
@@ -0,0 +1,19 @@
+let quote = "";
+
+function fetchQuote() {
+  fetch(" https://api.quotable.io/random?tags=technology")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("result").innerHTML =
+        `<code class = "text-white">&#8221</code>` +
+        data.content +
+        `<code class = "text-white">&#8221 </code>`;
+
+      document.getElementById("author").innerHTML =
+        `<code class="text-white">&#8212; </code>` +
+        data.author +
+        `<code class="text-white"> &#8212; </code>`;
+    });
+}
+fetchQuote();
+setInterval(fetchQuote, 30000);
