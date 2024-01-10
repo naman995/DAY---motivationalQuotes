880ad6b9e805243a4c52686cb9a1af1ba9a3b2f8
naman995 <rajputnaman823@gmail.com>
2024-01-10 10:44:53 +0530

    ⚡️

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-let quote = "";
-
 function fetchQuote() {
   fetch(" https://api.quotable.io/random?tags=technology")
     .then((response) => response.json())
     .then((data) => {
       document.getElementById("result").innerHTML =
-        `<code class = "text-white">&#8221</code>` +
+        `<code class = "text-white">&#8220</code>` +
         data.content +
         `<code class = "text-white">&#8221 </code>`;
 
